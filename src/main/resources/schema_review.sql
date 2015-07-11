5a76b89315e34090f335c68163029c5d57e6aa57
Ryozo Watanabe <clearstor@gmail.com>
2015-07-11 15:42:25 +0900

    テーブル定義を修正。別DataSourceの情報はIDではなくビジネスキーで保持する

--- src/main/resources/schema_review.sql
+++ src/main/resources/schema_review.sql
@@ -3,8 +3,8 @@ CREATE TABLE reviews(
   id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   evaluation INT NOT NULL,
   comment VARCHAR(255),
+  product_code char(10) NOT NULL,
   customer_id BIGINT NOT NULL,
-  product_id BIGINT NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
   version BIGINT NOT NULL
